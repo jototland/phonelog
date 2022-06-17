cc2b456f9a8da023cc52f863fa6a059f3858cef3
Jo Totland <jototland@gmail.com>
2022-06-17 14:47:05 +0200

    bugfix datepicker

--- app/static/history.js
+++ app/static/history.js
@@ -1,17 +1,26 @@
-const start_date = document.getElementById('start_date')
+document.addEventListener('DOMContentLoaded', () => {
+    const start_date = document.getElementById('start_date')
+    const date = new Date(start_date.dataset.from * 1000)
+    const year = String(1900 + date.getYear()).padStart(4, '0')
+    const month = String(1 + date.getMonth()).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    if (hour == 0 && minute == 0 && second == 0) {
+        iso_date = year + '-' + month + '-' + day
+        start_date.setAttribute('value', iso_date)
+    }
 
-start_date.setAttribute(
-    'value',
-    new Date(start_date.dataset.from * 1000).toISOString().substring(0,10)
-)
-
-
-start_date.addEventListener("change", () => {
-    from_epoch = (+new Date(start_date.value+"T00:00:00")) / 1000
-    to_epoch = from_epoch + 24*60*60
-    window.location.href =
-        window.location.origin +
-        window.location.pathname +
-        '?from=' + from_epoch +
-        '&to=' + to_epoch
+    start_date.addEventListener("change", () => {
+        from_epoch = (+new Date(start_date.value+"T00:00:00")) / 1000
+        to_epoch = from_epoch + 24*60*60
+        window.location.href =
+            window.location.origin +
+            window.location.pathname +
+            '?from=' + from_epoch +
+            '&to=' + to_epoch
+    })
 })
+
+
